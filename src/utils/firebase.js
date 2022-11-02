8904ef2d58b41d0f047c97965bd069952296612a
Carlos A. Garcia Gonzales <cgarcia@stsac.com>
2022-11-01 23:23:02 -0500

    Agregando componente GetAuth

---
+++ src/utils/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3p8ZtFCvkgg_HIiWYDEZWLIoDxKEGd9k",
+  authDomain: "musicfy-v2-fd13d.firebaseapp.com",
+  projectId: "musicfy-v2-fd13d",
+  storageBucket: "musicfy-v2-fd13d.appspot.com",
+  messagingSenderId: "235205026428",
+  appId: "1:235205026428:web:a9a37d5feb7d40115f1ff9"
+};
+
+initializeApp(firebaseConfig);
